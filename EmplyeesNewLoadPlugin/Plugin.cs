d7412f993672ed016c05788462fca90340b9fc1e
Иванов Александр <sana-ivanov1@yandex.ru>
2023-07-27 20:46:04 +0500

    add libs

---
+++ EmplyeesNewLoadPlugin/Plugin.cs
@@ -0,0 +1,36 @@
+﻿using PhoneApp.Domain.Attributes;
+using PhoneApp.Domain.DTO;
+using PhoneApp.Domain.Interfaces;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+
+namespace EmplyeesNewLoadPlugin
+{
+    [Author(Name = "Alexandr Ivanov")]
+    public class Plugin : IPluggable
+    {
+        private static readonly NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
+        public IEnumerable<DataTransferObject> Run(IEnumerable<DataTransferObject> args)
+        {
+            logger.Info("Прогрузился");
+
+            var employeesList = Newtonsoft.Json.JsonConvert.DeserializeObject<Root>(File.ReadAllText("data.json"));
+
+            List<EmployeesDTO> employees = new List<EmployeesDTO>();
+
+            foreach (var user in employeesList.Users)
+            {
+                employees.Add(new EmployeesDTO()
+                {
+                    Name = $"{user.FirstName}  {user.LastName}"
+                });
+            }
+
+            logger.Info($"Loaded {employeesList.Users.Count()} employees");
+
+            return employees.Cast<DataTransferObject>();
+        }
+    }
+}
